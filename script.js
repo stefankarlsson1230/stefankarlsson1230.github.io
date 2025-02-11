3c165d9f31ab23b85e46a34d1786ce685d4a3abb
stefankarlsson1230@gmail.com <stefankarlsson1230@gmail.com>
2025-02-11 10:41:01 +0100

    preliminary main menu done

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+// Get all menu links
 const menuItems = document.getElementsByClassName("menuLink");
 
-// menuItems is a bunch of items, I have to do something like this
-// for (var i = 0 ; i < comment.length; i++) {
-//     comment[i].addEventListener('click' , showComment , false ) ; 
-// }
+// Add event listeners to all menu links
+for(let i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener("click", clickMenuItem);
+}
 
+// Update apperance of menu links when clicked
+function clickMenuItem() {
+    for(let i = 0; i < menuItems.length; i++) {
+        menuItems[i].classList.remove("selectedMenuItem");
+        menuItems[i].classList.add("unselectedMenuItem");
+    }
 
-.addEventListener("click", function (e) {
-    console.log(e);
-
-
-});
+    this.classList.remove("unselectedMenuItem");
+    this.classList.add("selectedMenuItem");
+}
 
